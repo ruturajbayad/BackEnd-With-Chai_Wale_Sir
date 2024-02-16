a20dac1b42935c8b4f9bb590c7139fc55f8a6460
Ruturaj Bayad <ruturajsinhbayad@gmail.com>
2024-02-16 12:23:56 +0530

    Chnage in index js TO mention Hitesh Sir

--- Basics/index.js
+++ Basics/index.js
@@ -10,6 +10,10 @@ app.get('/Profile', (req, res) => {
     res.send('<h1>Ruturaj Bayad</h1>')
 })
 
+app.get('/Chai_Wale_Sir', (req, res) => {
+    res.send("<h1> Following Hitesh Sir (I write Code) 👍🚀 </h1>")
+})
+
 app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
 });
